20904469e1e958d06d5d8799ddad60ba08b5cec2
Elias Vestermark <eliasvester96@gmail.com>
2024-01-16 11:19:56 +0100

    Add project files.

---
+++ DatalagringUppgift/Data/CreateTableProductCatalog.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABlE Categories
+(
+	CategoryId int not null identity primary key,
+	Name nvarchar(20)
+)
+
+CREATE TABlE Ingridients
+(
+	IngridientId int not null identity primary key,
+	Name nvarchar(50)
+)
+GO
+
+CREATE TABLE Products
+(
+	ProductId int not null identity primary key,
+	Name nvarchar(50) not null,
+	Price money not null, 
+	CategoryId int not null references Categories(CategoryId)
+)
+GO
+
+CREATE TABLE ProductIngridients
+(
+	ProductId int not null references Products(ProductId),
+	IngridientId int not null references Ingridients(IngridientId)
+
+	primary key (ProductId, IngridientId)
+)
